8fd2a8fcd03a07013d8fd2e23c1b5610ff7f1079
manuel <manuel@manuel-ESPRIMO-P>
2017-01-17 10:54:55 +0100

    tercera pujdada

--- src/projectem3/Memoria.java
+++ src/projectem3/Memoria.java
@@ -11,10 +11,61 @@ package projectem3;
  */
 public class Memoria {
 
-    String tipus = null;
-    String marca = null;
-    int id = 0;
-    boolean ssd = false;
-    boolean omplit = false;
+    private String tipus = null;
+    private String marca = null;
+    private double preu = 0;
+    private boolean ssd = false;
+    private boolean omplit = false;
+
+    @Override
+    public String toString() {
+        return "Memoria{" + "tipus=" + tipus + ", marca=" + marca + ", preu=" + preu + ", ssd=" + ssd + '}';
+    }
+
+    public String getTipus() {
+        return tipus;
+    }
+
+    public void setTipus(String tipus) {
+        this.tipus = tipus;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public double getPreu() {
+        return preu;
+    }
+
+    public void setPreu(double preu) {
+        if (preu < 0) {
+            System.out.println("Els preus no poden ser negatius");
+        } else {
+            this.preu = (int) preu;
+        }
+
+        this.preu = (int) preu;
+    }
+
+    public boolean isSsd() {
+        return ssd;
+    }
+
+    public void setSsd(boolean ssd) {
+        this.ssd = ssd;
+    }
+
+    public boolean isOmplit() {
+        return omplit;
+    }
+
+    public void setOmplit(boolean omplit) {
+        this.omplit = omplit;
+    }
 
 }
--- src/projectem3/Projectem3.java
+++ src/projectem3/Projectem3.java
@@ -6,18 +6,69 @@
 
 package projectem3;
 
+import java.util.Scanner;
+
 /**
  *
  * @author manuel
  */
 public class Projectem3 {
 
-        // TODO code application logic here
+    public static final int MAX_MEMORIES=2;
+    
+    
+    public static void main(String[] args){
+        // Array on guardem informació de les memòries
+        
+        Memoria[] array=new Memoria[MAX_MEMORIES];
+        
+        for (int i = 0; i < array.length; i++) {
+            array[i]=new Memoria();
+        }
+        
+        int i = 0;
+        String s=new String("");s="";
+        
+        Memoria HDSata=new Memoria();
+        HDSata.setTipus("Seagate");
+        array[0]=new Memoria();
+        array[0].setTipus("Western");
+        array[0].setPreu(200);
+        array[0].setSsd(false);
+        array[0].setOmplit(true);
+        
+        System.out.println(array[0]);
+        //System.out.println("La memòria" +HDSata.getTipus()+" val" +HDSata.getPreu());
+        System.out.println("La memòria" +array[0].getTipus()+" val" +HDSata.getPreu());
+        String n=array[1].getTipus();
+        System.out.println();
+        
+        
+        
+        System.exit(0);
+        
+        //Les meves propietats
         String tipus = null;
         String marca = null;
-        int id = 0;
+        int preu = 0;
         boolean ssd = false;
         boolean omplit = false;
-    }
-    
+        
+        
+        Scanner ent = new Scanner(System.in);
+        int opcio;
+        
+        /*
+        do {
+            System.out.println("\n\nMenú de l'aplicació.");
+            System.out.println("0. Sortir.");
+            System.out.println("1. Introduïr memoria.");
+            System.out.println("2. Introduir tipus ");
+            System.out.println("3. Modificar memoria.");
+            System.out.println("4. Borrar memoria.");
+            System.out.println("5. Llistar memoria.");
+            System.out.println("6. Recuperar memoria borrada.");
+    */
+}
+        }
 
